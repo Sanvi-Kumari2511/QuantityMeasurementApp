3e4ad80595e75a3a155d05fab2908770c1303430
Sanvi <kumarisanvi2511@gmail.com>
2026-02-20 17:27:15 +0530

    [Sanvi] : Added quantitymeasurement

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,21 +2,21 @@ package com.apps.quantitymeasurement;
 
 import java.util.Scanner;
 
-//Main class
+// Main class
 public class QuantityMeasurementApp {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter two feet values:");
+        System.out.println("Enter two feet values:");
 
-		Feet feet1 = new Feet(sc.nextDouble());
-		Feet feet2 = new Feet(sc.nextDouble());
+        Feet feet1 = new Feet(sc.nextDouble());
+        Feet feet2 = new Feet(sc.nextDouble());
 
-		// Comparing values
-		System.out.println("Both Feet equal? " + feet1.equals(feet2));
+        // Comparing values
+        System.out.println("Both Feet equal? " + feet1.equals(feet2));
 
-		sc.close();
-	}
+        sc.close();
+    }
 }
