bd91368b4ae64f720e3a6efb8770297c65c5ca9e
Sanvi <kumarisanvi2511@gmail.com>
2026-02-23 16:44:04 +0530

    [Sanvi] : Added UC8: Refactoring Unit Enum to Standalone with Conversion Responsibility

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,7 +1,5 @@
 package com.apps.quantitymeasurement;
 
-import com.apps.quantitymeasurement.Length.LengthUnit;
-
 public class QuantityMeasurementApp {
 
 	// UC1-UC-4 : Equality
@@ -11,12 +9,6 @@ public class QuantityMeasurementApp {
 
 	public static boolean demonstrateLengthComparison(double v1, LengthUnit u1, double v2, LengthUnit u2) {
 		return demonstrateLengthEquality(new Length(v1, u1), new Length(v2, u2));
-	}
-
-	// raw values
-	public static double demonstrateLengthConversion(double value, LengthUnit fromUnit, LengthUnit toUnit) {
-
-		return Length.convert(value, fromUnit, toUnit);
 	}
 
 	// UC-5 : Conversion(Overloaded)
@@ -30,18 +22,21 @@ public class QuantityMeasurementApp {
 	}
 
 	// UC7 : Target Addition
-	public static Length demonstrateLengthAddition(Length l1, Length l2, Length.LengthUnit targetUnit) {
+	public static Length demonstrateLengthAddition(Length l1, Length l2, LengthUnit targetUnit) {
 		return l1.add(l2, targetUnit);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(
-				"1 ft == 12 in ? " + demonstrateLengthComparison(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCHES));
 
-		System.out.println(
-				"convert(3 yards -> feet) = " + demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET));
+		System.out.println("===== UC8 DEMO OUTPUT =====");
 
-		Length sum = demonstrateLengthAddition(new Length(1.0, LengthUnit.FEET), new Length(12.0, LengthUnit.INCHES));
-		System.out.println("1 ft + 12 in = " + sum);
+		System.out.println(new Length(1, LengthUnit.FEET).convertTo(LengthUnit.INCHES));
+		System.out.println(new Length(1, LengthUnit.FEET).add(new Length(12, LengthUnit.INCHES), LengthUnit.FEET));
+
+		System.out.println(new Length(36, LengthUnit.INCHES).equals(new Length(1, LengthUnit.YARDS)));
+
+		System.out.println(new Length(1, LengthUnit.YARDS).add(new Length(3, LengthUnit.FEET), LengthUnit.YARDS));
+
+		System.out.println(new Length(2.54, LengthUnit.CENTIMETERS).convertTo(LengthUnit.INCHES));
 	}
 }
