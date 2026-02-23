1add5c456c57accd3f20ad15fdb612abe32baae1
Sanvi <kumarisanvi2511@gmail.com>
2026-02-23 11:39:23 +0530

    [Sanvi] : Added UC2: Feet and Inches measurement equality

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,22 +1,25 @@
 package com.apps.quantitymeasurement;
 
-import java.util.Scanner;
-
-//Main class
+//main class
 public class QuantityMeasurementApp {
+	public static boolean compareFeet(double value1, double value2) {
+		Feet f1 = new Feet(value1);
+		Feet f2 = new Feet(value2);
+		return f1.equals(f2);
+	}
+
+	public static boolean compareInches(double value1, double value2) {
+		Inches i1 = new Inches(value1);
+		Inches i2 = new Inches(value2);
+		return i1.equals(i2);
+	}
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		System.out.println("Input: 1.0 inch and 1.0 inch");
+		System.out.println("Equal (" + compareInches(1.0, 1.0) + ")");
 
-		System.out.println("Enter two feet values:");
-
-		Feet feet1 = new Feet(sc.nextDouble());
-		Feet feet2 = new Feet(sc.nextDouble());
-
-		// Comparing values
-		System.out.println("Both Feet equal? " + feet1.equals(feet2));
-
-		sc.close();
+		System.out.println("Input: 1.0 ft and 1.0 ft");
+		System.out.println("Equal (" + compareFeet(1.0, 1.0) + ")");
 	}
 }
