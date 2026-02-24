15a8a525607457009a49ae8925be359e612b9342
Sanvi <kumarisanvi2511@gmail.com>
2026-02-24 10:38:11 +0530

    [Sanvi] : Added UC9: Weight Measurement Equality, Conversion, and Addition (Kilogram, Gram, Pound)

--- src/main/java/com/apps/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/apps/quantitymeasurement/WeightUnit.java
@@ -1,15 +1,16 @@
 package com.apps.quantitymeasurement;
 
-public enum LengthUnit {
+public enum WeightUnit {
 
-	FEET(1.0), 
-	INCHES(1.0 / 12.0), 
-	YARDS(3.0), 
-	CENTIMETERS(1.0 / 30.48);
+	MILLIGRAM(0.001), 
+	GRAM(1.0), 
+	KILOGRAM(1000.0), 
+	POUND(453.592), 
+	TONNE(1_000_000.0);
 
 	private final double conversionFactor;
 
-	LengthUnit(double conversionFactor) {
+	WeightUnit(double conversionFactor) {
 		this.conversionFactor = conversionFactor;
 	}
 
@@ -17,18 +18,17 @@ public enum LengthUnit {
 		return conversionFactor;
 	}
 
+	// Convert this unit → base unit (grams)
 	public double convertToBaseUnit(double value) {
-		if (!Double.isFinite(value)) {
+		if (!Double.isFinite(value))
 			throw new IllegalArgumentException("Invalid value");
-		}
 		return Math.round(value * conversionFactor * 100.0) / 100.0;
 	}
 
+	// Convert base unit (grams) → this unit
 	public double convertFromBaseUnit(double baseValue) {
-		if (!Double.isFinite(baseValue)) {
+		if (!Double.isFinite(baseValue))
 			throw new IllegalArgumentException("Invalid value");
-		}
 		return Math.round((baseValue / conversionFactor) * 100.0) / 100.0;
 	}
-
 }
