ef70adef93fd98ae0e8f3ff1e628c78b93626122
Sanvi <kumarisanvi2511@gmail.com>
2026-04-17 15:10:42 +0530

    [Sanvi] : Added Modified files

--- src/main/java/com/apps/quantitymeasurement/config/SecurityConfig.java
+++ src/main/java/com/apps/quantitymeasurement/config/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.csrf(csrf -> csrf.disable())
+		http
+		.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/auth/**").permitAll().requestMatchers("/api/**").authenticated())
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
